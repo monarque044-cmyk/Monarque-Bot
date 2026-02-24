558aafc39148931aded0d0109c0f361f4308384a
monarque044-cmyk <diasmael044@gmail.com>
2026-02-24 22:16:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ const __dirname = path.dirname(__filename);
 // ================== CONFIGURATION PAR DÉFAUT ==================
 const defaultConfig = {
   // 🔑 Identifiants
-  SESSION_ID: "kaya~jR9AWTSZ#-2uMMpzJkXUwFeo9MdBZwtd_gKc2djIMQtTIDzTRSoI",
-  OWNERS: ["243910014043"], // ← tableau des owners, uniquement les numéros
+  SESSION_ID: "monarque~jR8AWTSZ#-3uMMpzJkXUwFeo8MdBZwtd_gKc3djIMQtTIDzTRSoI",
+  OWNERS: ["22789081884"], // ← tableau des owners, uniquement les numéros
   PREFIX: ".",
   TIMEZONE: "Africa/Kinshasa",
   VERSION: "2.0.0",
