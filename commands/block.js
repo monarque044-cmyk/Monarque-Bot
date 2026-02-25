8a1a6790a2d8c81ba15741f61c91c2ea64dca5ec
monarque044-cmyk <diasmael044@gmail.com>
2026-02-25 10:24:04 +0100

    Update block.js

--- commands/block.js
+++ commands/block.js
@@ -3,13 +3,13 @@ export default {
   name: "block",
   category: "Owner",
 
-  run: async (kaya, m) => {
+  run: async (monarque, m) => {
     try {
       // 🔐 Owner uniquement (en MP)
       if (!m.fromMe || m.isGroup) return;
 
       // 🔒 Blocage direct, silencieux
-      await kaya.updateBlockStatus(m.chat, "block");
+      await monarque.updateBlockStatus(m.chat, "block");
 
     } catch {
       // ❌ AUCUNE sortie, AUCUN log
